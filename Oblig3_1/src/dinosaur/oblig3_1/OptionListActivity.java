0a6d79a55f34ae73e434bd30578e4ed58b08d26f
Ken-HAvard Lieng <kk_998@hotmail.com>
2013-03-11 16:54:23 +0100

    Nye hakekors og kategorier.

--- Oblig3_1/src/dinosaur/oblig3_1/OptionListActivity.java
+++ Oblig3_1/src/dinosaur/oblig3_1/OptionListActivity.java
@@ -1,23 +1,86 @@
 package dinosaur.oblig3_1;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OptionListActivity extends ListActivity {
+	private int[] CATEGORIES = new int[] {
+			R.array.category_telephony,
+			R.array.category_network,
+			R.array.category_div
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_option_list);
+		
+		int categoryIndex = getIntent().getExtras().getInt(MainActivity.CATEGORY_INDEX);
+		String category = getResources().getStringArray(R.array.categories)[categoryIndex];
+		
+		setTitle("Kategori - " + category);
+		
+		String[] options = getResources().getStringArray(CATEGORIES[categoryIndex]);
+		getListView().setAdapter(new OptionAdapter(this, R.layout.row_option, options));
+		
+		Button buttonSave = (Button)findViewById(R.id.button_save);
+		buttonSave.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// Skyv innstillinger til DB her
+				finish();
+			}			
+		});
 	}
 
+	private void foo(String s) {
+		Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_option_list, menu);
 		return true;
 	}
 
+	private class OptionAdapter extends ArrayAdapter<String> {
+		private String[] entries;
+		
+		public OptionAdapter(Context context, int textViewResourceId, String[] entries) {
+			super(context, textViewResourceId, entries);
+			this.entries = entries;
+		}
+		
+		@Override
+        public View getView(final int position, View view, ViewGroup parent) {
+            final View v = getLayoutInflater().inflate(R.layout.row_option, null);
+            TextView dt = (TextView)v.findViewById(R.id.status);
+            dt.setFocusable(false);
+            dt.setFocusableInTouchMode(false);
+            dt.setText(entries[position]);
+            
+            final CheckBox cb = (CheckBox)v.findViewById(R.id.checkbox);
+            cb.setOnClickListener(new OnClickListener() {
+    			@Override
+    			public void onClick(View arg0) {
+    				foo(entries[position] + " " + cb.isChecked());
+    			}			
+    		});
+            
+            return v;
+		}
+	}
 }
